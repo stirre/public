3c830c0a5df891c9513762bac9e8f676c897c437
stirre <78666388+stirre@users.noreply.github.com>
2021-03-23 07:45:27 +0200

    Update minecraft.sh

--- 20.04/minecraft.sh
+++ 20.04/minecraft.sh
@@ -1,6 +1,7 @@
 apt install -y screen default-jdk nmap
 sudo useradd -m -r -d /opt/minecraft minecraft
 mkdir /opt/minecraft/survival
+cd /opt/minecraft/survival
 https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 sudo bash -c "echo eula=true > /opt/minecraft/survival/eula.txt" 
 sudo chown -R minecraft /opt/minecraft/survival/
