ff708889d8069d7e0fc8d3bc42de9d039a0c90a8
stirre <78666388+stirre@users.noreply.github.com>
2021-03-23 07:44:01 +0200

    Update minecraft.sh

--- 20.04/minecraft.sh
+++ 20.04/minecraft.sh
@@ -1,4 +1,4 @@
-apt install screen default-jdk nmap
+apt install -y screen default-jdk nmap
 sudo useradd -m -r -d /opt/minecraft minecraft
 mkdir /opt/minecraft/survival
 https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
